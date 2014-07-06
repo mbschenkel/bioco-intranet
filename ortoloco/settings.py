fb7d0d06d8316cc0889e186a77a79accbc988784
Markus B. Schenkel <github@markus-schenkel.ch>
2014-07-06 23:12:27 +0200

    adding allowed hosts

--- ortoloco/settings.py
+++ ortoloco/settings.py
@@ -84,7 +84,12 @@ AUTHENTICATION_BACKENDS = (
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['intranet.bioco.ch']
+ALLOWED_HOSTS = [
+    'intranet.bioco.ch',
+    'intranet-test.bioco.ch',
+    'ortho-bioco.rhcloud.com',
+    'test-bioco.rhcloud.com'
+]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
