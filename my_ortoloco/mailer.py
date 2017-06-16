c65b23c7ab0df187c8c2fb91d82afca3e07126c6
Markus B. Schenkel <github@markus-schenkel.ch>
2017-06-16 18:31:18 +0200

    Corrected from mail address format

--- my_ortoloco/mailer.py
+++ my_ortoloco/mailer.py
@@ -8,7 +8,7 @@ from django.core.mail import EmailMultiAlternatives
 import re
 
 #single point of change
-SENDER_EMAIL_ADDRESS = '<' + settings.EMAIL_HOST_USER + '> ' + settings.SITE_MY_NAME
+SENDER_EMAIL_ADDRESS = settings.SITE_MY_NAME + '<' + settings.EMAIL_HOST_USER + '> '
 
 # sends mail only to specified email-addresses if dev mode
 def send_mail(subject, message, from_email, to_emails):
