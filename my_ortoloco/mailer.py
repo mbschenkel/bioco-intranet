1cb5b1dfd6d79ae2cb865f2321379c3059030715
Markus B. Schenkel <github@markus-schenkel.ch>
2017-07-04 21:00:20 +0200

    Implemented non-django based mailer for plain text messages

--- my_ortoloco/mailer.py
+++ my_ortoloco/mailer.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+# Import smtplib for the actual sending function
+import smtplib
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 from django.conf import settings
 from django.core import mail
 from django.template.loader import get_template
@@ -7,9 +13,8 @@ from django.template import Context
 from django.core.mail import EmailMultiAlternatives
 import re
 
-#single point of change
-#SENDER_EMAIL_ADDRESS = settings.SITE_MY_NAME + '<' + settings.EMAIL_HOST_USER + '> '
-SENDER_EMAIL_ADDRESS = settings.EMAIL_HOST_USER
+# single point of change
+SENDER_EMAIL_ADDRESS = settings.SITE_MY_NAME + ' <' + settings.EMAIL_HOST_USER + '> '
 
 # sends mail only to specified email-addresses if dev mode
 def send_mail(subject, message, from_email, to_emails):
@@ -19,17 +24,39 @@ def send_mail(subject, message, from_email, to_emails):
     else:
         # in debug mode send to dummy address
         okmails = [settings.DEBUG_EMAIL_ADDRESS]
-        print "Mail intended for " + ", ".join(to_emails).encode('utf-8') + " rerouted to " + settings.DEBUG_EMAIL_ADDRESS.encode('utf-8')
-    
+        print "Mail intended for " + ", ".join(to_emails).encode(
+            'utf-8') + " rerouted to " + settings.DEBUG_EMAIL_ADDRESS.encode('utf-8')
+
+    server = smtplib.SMTP(settings.EMAIL_HOST)
+    # server.set_debuglevel(1)
+    status_login = server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+
     if len(okmails) > 0:
         for amail in okmails:
-            res = mail.send_mail(subject, message, from_email, [amail], fail_silently=False)
-            print 'Sending mail from ' + from_email.encode('utf-8') + ' to ' + amail.encode('utf-8') 
-            print ' res= ', res
-        
+            # Original django-only mailer, failing on openshift
+            # res = mail.send_mail(subject, message, from_email, [amail], fail_silently=False)
+
+            msg = MIMEText(message.encode('utf-8'), 'plain', 'utf-8')
+            msg['Subject'] = subject.encode('utf-8')
+            msg['From'] = from_email
+            msg['To'] = amail
+            res = server.sendmail(from_email, [amail], msg.as_string())
+
+            print 'Sending mail from ' + from_email.encode('utf-8') + ' to ' + amail.encode('utf-8')
+            print '-> res= ', res
+
     # todo remove again later, for now send a copy every time
-    message_verbose =  message + '\n\nCopy of original sent to: ' + ', '.join(to_emails)
-    res = mail.send_mail(subject, message_verbose, from_email, [settings.DEBUG_EMAIL_ADDRESS], fail_silently=False)
+    message_verbose = (message + '\n\nCopy of original sent to: ' + ', '.join(to_emails)).encode('utf-8')
+    msg_debug = MIMEText(message_verbose, 'plain', 'utf-8')
+    msg_debug['Subject'] = subject.encode('utf-8')
+    msg_debug['From'] = from_email
+    msg_debug['To'] = settings.DEBUG_EMAIL_ADDRESS
+    server.sendmail(from_email, [settings.DEBUG_EMAIL_ADDRESS], msg_debug.as_string())
+
+    # Original django-only mailer, failing on openshift
+    # res = mail.send_mail(subject, message_verbose, from_email, [settings.DEBUG_EMAIL_ADDRESS], fail_silently=False)
+
+    server.quit()
 
     return None
 
@@ -49,25 +76,28 @@ def send_mail_multi(email_multi_message):
         okmails = email_multi_message.to
     else:
         okmails = [settings.DEBUG_EMAIL_ADDRESS]
-        print "Multi-Mail intended for " + ", ".join(to_emails).encode('utf-8') + " rerouted to " + settings.DEBUG_EMAIL_ADDRESS.encode('utf-8')
-        
+        print "Multi-Mail intended for " + ", ".join(to_emails).encode(
+            'utf-8') + " rerouted to " + settings.DEBUG_EMAIL_ADDRESS.encode('utf-8')
+
     if len(okmails) > 0:
         email_multi_message.to = []
         email_multi_message.bcc = okmails
         res = email_multi_message.send()
         print "res = ", res
         print "Mail sent to " + ", ".join(okmails).encode('utf-8') + (", on whitelist" if settings.DEBUG else "")
-        
+
     # todo remove again later, for now send a copy every time
-    message_verbose =  email_multi_message.body + '\n\nCopy of original sent to: ' + ', '.join(to_emails)
-    res = mail.send_mail(email_multi_message.subject, message_verbose, email_multi_message.from_email, [settings.DEBUG_EMAIL_ADDRESS], fail_silently=False)
+    message_verbose = email_multi_message.body + '\n\nCopy of original sent to: ' + ', '.join(to_emails)
+    res = mail.send_mail(email_multi_message.subject, message_verbose, email_multi_message.from_email,
+                         [settings.DEBUG_EMAIL_ADDRESS], fail_silently=False)
 
     return None
 
 
 def send_new_loco_in_taetigkeitsbereich_to_bg(area, loco):
     send_mail('Neues Mitglied im Taetigkeitsbereich ' + area.name,
-              'Soeben hat sich ' + loco.first_name + " " + loco.last_name + ' in den Taetigkeitsbereich ' + area.name + ' eingetragen', SENDER_EMAIL_ADDRESS, [area.coordinator.email])
+              'Soeben hat sich ' + loco.first_name + " " + loco.last_name + ' in den Taetigkeitsbereich ' + area.name + ' eingetragen',
+              SENDER_EMAIL_ADDRESS, [area.coordinator.email])
 
 
 def send_contact_form(subject, message, loco, copy_to_loco):
@@ -196,9 +226,9 @@ def send_job_reminder(emails, job, participants, server):
     plaintext = get_template('mails/job_reminder_mail.txt')
     htmly = get_template('mails/job_reminder_mail.html')
     vcal_template = get_template('mails/cal.ical')
-    
+
     subject = settings.SITE_NAME + " - Job-Erinnerung"
-    
+
     d = Context({
         'subject': subject,
         'job': job,
@@ -215,14 +245,15 @@ def send_job_reminder(emails, job, participants, server):
     msg.attach_alternative(html_content, "text/html")
     msg.attach_alternative(vcal_content, "text/calendar")
     send_mail_multi(msg)
+
 
 def send_job_signup(emails, job, participants, server):
     plaintext = get_template('mails/job_signup_mail.txt')
     htmly = get_template('mails/job_signup_mail.html')
     vcal_template = get_template('mails/cal.ical')
-    
+
     subject = settings.SITE_NAME + " - Job-Anmeldung"
-    
+
     d = Context({
         'subject': subject,
         'job': job,
@@ -234,8 +265,9 @@ def send_job_signup(emails, job, participants, server):
     text_content = plaintext.render(d)
     html_content = htmly.render(d)
     vcal_content = vcal_template.render(d)
-    
+
     msg = EmailMultiAlternatives(subject, text_content, SENDER_EMAIL_ADDRESS, emails)
     msg.attach_alternative(html_content, "text/html")
     msg.attach_alternative(vcal_content, "text/calendar")
     send_mail_multi(msg)
+
