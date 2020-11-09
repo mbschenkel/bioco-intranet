9ca5eb1a0537471f340bde0ee2aed768d43425f1
Markus B. Schenkel <github@markus-schenkel.ch>
2020-11-09 23:24:24 +0100

    Revert

--- bioco/urls.py
+++ bioco/urls.py
@@ -48,9 +48,4 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', admin.site.urls),
-
-    # Statics for heroku
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT,
-    }),
 ]
